3bc99b438b3e210c9fcdc13b26b168f9daabdf22
Melarri Kenn Salazar <melarrisalazar926@gmail.com>
2026-08-20 15:20:21 +0800

    Load middleware config during bootstrap

--- app/config/config.php
+++ app/config/config.php
@@ -348,4 +348,9 @@ $config['csrf_token_name']         = 'csrf_test_name';
 $config['csrf_cookie_name']        = 'csrf_cookie_name';
 $config['csrf_expire']             = 7200;
 $config['csrf_regenerate']         = FALSE;
+
+// Middleware is needed while the router is matching a request, before a
+// controller can initialize the normal application autoload configuration.
+require_once APP_DIR . 'middlewares/StudentMiddleware.php';
+require_once APP_DIR . 'config/middleware.php';
 ?>
