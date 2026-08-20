ce2193998a83b4975e9cbfa2908635aa70cb1581
Melarri Kenn Salazar <melarrisalazar926@gmail.com>
2026-08-20 15:32:47 +0800

    Block direct profile URL access

--- app/middlewares/StudentMiddleware.php
+++ app/middlewares/StudentMiddleware.php
@@ -9,7 +9,11 @@ class StudentMiddleware
             session_start();
         }
 
-        if (($_SESSION['student_access'] ?? false) !== true) {
+        $referrer = $_SERVER['HTTP_REFERER'] ?? '';
+        $referrer_path = parse_url($referrer, PHP_URL_PATH) ?: '';
+        $came_from_student_home = in_array(rtrim($referrer_path, '/'), ['', '/index.php', '/index.php/student'], true);
+
+        if (($_SESSION['student_access'] ?? false) !== true || !$came_from_student_home) {
             redirect('student');
             exit;
         }
